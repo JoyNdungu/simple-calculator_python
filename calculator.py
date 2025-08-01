312358b5663aeac7e0650064bb0f3c1c58e9791d
Joy <tessagrandeur@gmail.com>
2025-08-01 11:23:21 +0300

    Assignment1

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#simple calculator
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
+
+subtraction = num1 - num2
+print("The difference between", num1, "and", num2, "is", subtraction)
+
+multiplication = num1 * num2
+print("The product of", num1, "and", num2, "is", multiplication)
+
+division = num1 / num2
+print("The division of", num1, "and", num2, "is", division)
+
